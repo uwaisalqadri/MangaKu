8a9c050aea34bc4a7d9abd469d75aa6d234c5139
uwaisalqadri <uwaisalqadri654321@gmail.com>
2021-09-11 21:08:46 +0700

    detect drag gesture pt.3

--- iosApp/iosApp/App/Features/MyManga/MyMangaView.swift
+++ iosApp/iosApp/App/Features/MyManga/MyMangaView.swift
@@ -14,52 +14,56 @@ import ACarousel
 struct MyMangaView: View {
 
   @ObservedObject var viewModel: MyMangaViewModel
+  @State var itemWidth: CGFloat = 0.0
   @State var position = 0
 
   var body: some View {
-    NavigationView {
-      VStack {
-        Text("My Manga")
-          .font(.custom(.mbold, size: 23))
-          .padding(.top, -70)
-          .padding(.bottom, 30)
+    GeometryReader { view in
+      NavigationView {
+        VStack {
+          Text("My Manga")
+            .font(.custom(.mbold, size: 23))
+            .padding(.top, -70)
+            .padding(.bottom, 30)
 
-        if !viewModel.loading, !viewModel.mangas.isEmpty {
-          ZStack(alignment: Alignment(horizontal: .center, vertical: .top)) {
-            Text(viewModel.mangas[position].getTitle())
-              .font(.custom(.sedgwickave, size: 70))
-              .lineLimit(2)
-              .multilineTextAlignment(.center)
-              .padding(.top, -60)
+          if !viewModel.loading, !viewModel.mangas.isEmpty {
+            ZStack(alignment: Alignment(horizontal: .center, vertical: .top)) {
+              Text(viewModel.mangas[position].getTitle())
+                .font(.custom(.sedgwickave, size: 70))
+                .lineLimit(2)
+                .multilineTextAlignment(.center)
+                .padding(.top, -60)
 
-            ACarousel(
-              viewModel.mangas,
-              id: \.self,
-              spacing: 20,
-              headspace: 50,
-              isWrap: true) { manga in
+              ACarousel(
+                viewModel.mangas,
+                id: \.self,
+                spacing: 20,
+                headspace: 50,
+                isWrap: true) { manga in
 
-              WebImage(url: URL(string: manga.attributes?.posterImage?.original ?? ""))
-                .resizable()
-                .indicator(.activity)
-                .cornerRadius(12)
+                WebImage(url: URL(string: manga.attributes?.posterImage?.original ?? ""))
+                  .resizable()
+                  .indicator(.activity)
+                  .cornerRadius(12)
 
-            }.frame(height: 400)
-            .padding(.top, 70)
-//            .simultaneousGesture(
-//              DragGesture().onEnded(onDragEnded)
-//            )
+              }.frame(height: 400)
+              .padding(.top, 70)
+              .simultaneousGesture(
+                DragGesture().onEnded(onDragEnded)
+              )
+            }
           }
-        }
 
-        Spacer()
+          Spacer()
+        }
+      }.onAppear {
+        itemWidth = view.size.height
       }
     }
   }
 
   private func onDragEnded(drag: DragGesture.Value) {
-    let dragThreshold: CGFloat = 200
-    if drag.predictedEndTranslation.width > dragThreshold || drag.translation.width > dragThreshold {
+    if drag.predictedEndTranslation.width > itemWidth || drag.translation.width > itemWidth {
 
       if position != viewModel.mangas.startIndex {
         position = position - 1
@@ -67,7 +71,7 @@ struct MyMangaView: View {
         position = viewModel.mangas.count - 1
       }
 
-    } else if (drag.predictedEndTranslation.width) < (-1 * dragThreshold) || (drag.translation.width) < (-1 * dragThreshold) {
+    } else if (drag.predictedEndTranslation.width) < (-1 * itemWidth) || (drag.translation.width) < (-1 * itemWidth) {
 
       if position != viewModel.mangas.count - 1 {
         position = position + 1
