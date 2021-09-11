c099c41ed11970169dc98f45cdce9f4e364fa8bc
uwaisalqadri <uwaisalqadri654321@gmail.com>
2021-09-11 22:34:40 +0700

    move library to local

--- iosApp/iosApp/App/Features/MyManga/MyMangaView.swift
+++ iosApp/iosApp/App/Features/MyManga/MyMangaView.swift
@@ -17,6 +17,11 @@ struct MyMangaView: View {
   @State var itemWidth: CGFloat = 0.0
   @State var position = 0
 
+  var dragGesture: some Gesture {
+    DragGesture()
+      .onEnded(onDragEnded)
+  }
+
   var body: some View {
     GeometryReader { view in
       NavigationView {
@@ -39,7 +44,8 @@ struct MyMangaView: View {
                 id: \.self,
                 spacing: 20,
                 headspace: 50,
-                isWrap: true) { manga in
+                isWrap: true,
+                dragGesture: dragGesture as! DragGesture) { manga in
 
                 WebImage(url: URL(string: manga.attributes?.posterImage?.original ?? ""))
                   .resizable()
@@ -48,22 +54,21 @@ struct MyMangaView: View {
 
               }.frame(height: 400)
               .padding(.top, 70)
-              .simultaneousGesture(
-                DragGesture().onEnded(onDragEnded)
-              )
             }
           }
 
           Spacer()
         }
       }.onAppear {
-        itemWidth = view.size.height
+        itemWidth = view.size.width
       }
     }
   }
 
   private func onDragEnded(drag: DragGesture.Value) {
-    if drag.predictedEndTranslation.width > itemWidth || drag.translation.width > itemWidth {
+    let dragThreshold: CGFloat = itemWidth / 2
+
+    if drag.predictedEndTranslation.width > dragThreshold || drag.translation.width > dragThreshold {
 
       if position != viewModel.mangas.startIndex {
         position = position - 1
@@ -71,7 +76,7 @@ struct MyMangaView: View {
         position = viewModel.mangas.count - 1
       }
 
-    } else if (drag.predictedEndTranslation.width) < (-1 * itemWidth) || (drag.translation.width) < (-1 * itemWidth) {
+    } else if (drag.predictedEndTranslation.width) < (-1 * dragThreshold) || (drag.translation.width) < (-1 * dragThreshold) {
 
       if position != viewModel.mangas.count - 1 {
         position = position + 1
