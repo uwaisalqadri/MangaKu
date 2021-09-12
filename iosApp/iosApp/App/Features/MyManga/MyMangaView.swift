eabf4a8375a122e0d6181bed40ec04632b99720e
uwaisalqadri <uwaisalqadri654321@gmail.com>
2021-09-12 13:06:15 +0700

    adjusting mymanga view pt.2

--- iosApp/iosApp/App/Features/MyManga/MyMangaView.swift
+++ iosApp/iosApp/App/Features/MyManga/MyMangaView.swift
@@ -14,7 +14,6 @@ import ACarousel
 struct MyMangaView: View {
 
   @ObservedObject var viewModel: MyMangaViewModel
-  @State var itemWidth: CGFloat = 0.0
   @State var position: Int = 0
 
   var body: some View {
@@ -29,7 +28,7 @@ struct MyMangaView: View {
           if !viewModel.loading, !viewModel.mangas.isEmpty {
             ZStack(alignment: Alignment(horizontal: .center, vertical: .top)) {
               Text(viewModel.mangas[position].getTitle())
-                .font(.custom(.sedgwickave, size: 70))
+                .font(.custom(.sedgwickave, size: view.size.width / 7))
                 .lineLimit(2)
                 .multilineTextAlignment(.center)
                 .padding(.top, -60)
@@ -46,53 +45,51 @@ struct MyMangaView: View {
                   .indicator(.activity)
                   .cornerRadius(12)
 
-              }.frame(height: 400)
-              .padding(.top, 70)
-//              .simultaneousGesture(
-//                DragGesture().onEnded(onDragEnded)
-//              )
+              }.frame(height: view.size.height / 2)
+              .padding(.top, 50)
+              //.simultaneousGesture(
+              //  DragGesture().onEnded(onDragEnded)
+              //)
             }
+
+            HStack {
+              StarsView()
+                .padding(.leading, 70)
+
+              Spacer()
+
+            }.padding(.top, 10)
+
+            HStack {
+              VStack {
+                Text("Volume 72")
+                  .font(.custom(.mbold, size: 18))
+                  .padding(.leading, 70)
+
+                Text("04-08-2021")
+                  .font(.custom(.mmedium, size: 16))
+                  .padding(.leading, 70)
+              }
+
+              Spacer()
+
+              Button(action: {
+                print("remove")
+              }) {
+                Text("Remove Favorite")
+                  .foregroundColor(.white)
+                  .font(.custom(.mbold, size: 15))
+                  .padding(20)
+              }.background(Color.black)
+              .frame(height: 40)
+              .cornerRadius(10)
+              .padding(.trailing, 70)
+
+            }.padding(.top, 5)
           }
-
-          HStack {
-            StarsView()
-              .padding(.leading, 70)
-
-            Spacer()
-            
-          }.padding(.top, 10)
-
-          HStack {
-            VStack {
-              Text("Volume 72")
-                .font(.system(size: 18, weight: .bold))
-                .padding(.leading, 70)
-
-              Text("04-08-2021")
-                .font(.system(size: 16))
-                .padding(.leading, 70)
-            }
-
-            Spacer()
-
-            Button(action: {
-              print("remove")
-            }) {
-              Text("Remove Favorite")
-                .foregroundColor(.white)
-                .font(.system(size: 15, weight: .bold))
-                .padding(20)
-            }.background(Color.black)
-            .frame(height: 40)
-            .cornerRadius(10)
-            .padding(.trailing, 70)
-
-          }.padding(.top, 5)
 
           Spacer()
         }
-      }.onAppear {
-        itemWidth = view.size.width
       }
     }
   }
